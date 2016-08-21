963b94c5f8bd78d36c6fa2c49fc32a076b1801a5
hiyokon <world.wide.kotapon@me.com>
2016-08-21 18:54:52 +0900

    refactoring for plotly component's independency

--- app/modules/Dashboard/components/Chart.js
+++ app/modules/Dashboard/components/Chart.js
@@ -4,18 +4,14 @@ import Plotly from "react-plotlyjs"
 import keenDashboardsStyles from '../keen-dashboards.css';
 
 export default class Chart extends Component {
-  // componentDidMount() {
-  //   Plotly.newPlot(this.props.id, );
-  // }
-
   render() {
     return (
     <div className={keenDashboardsStyles.chartWrapper}>
       <div className={keenDashboardsStyles.chartTitle}>
-        {this.props.title}
+        {this.props.charttitle}
       </div>
       <div className={keenDashboardsStyles.chartStage}>
-        <Plotly data={this.props.store.getData()} layout={this.props.store.getLayout()} />
+        <Plotly data={this.props.chartdata} layout={this.props.layout} />
       </div>
       <div className={keenDashboardsStyles.chartNotes}>
         {this.props.notes}
--- app/modules/Dashboard/index.js
+++ app/modules/Dashboard/index.js
@@ -4,16 +4,18 @@ import Chart from './components/Chart';
 
 @observer(["dashboardStore"])
 export default class Dashboard extends Component {
+// componentDidMount() {
+// }
 
   render() {
     return (
     <div className="container-fluid">
       <div className="row">
         <div className="col-sm-8">
-          <Chart store={this.props.dashboardStore} id="1" title="1" notes="1"/>
+          <Chart store={this.props.dashboardStore} charttitle="1" notes="1" chartdata={this.props.dashboardStore.getData()} layout={this.props.dashboardStore.getLayout()} />/>
         </div>
         <div className="col-sm-4">
-          <Chart store={this.props.dashboardStore} id="2" title="1" notes="1"/>
+          <Chart store={this.props.dashboardStore} charttitle="1" notes="1" chartdata={this.props.dashboardStore.getData()} layout={this.props.dashboardStore.getLayout()} />/>
         </div>
       </div>
     </div>
